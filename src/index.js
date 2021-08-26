30ddd0995ee4647a8085256f40a33eca8d0ff248
JavIonBek <javlonbekshamsiddinov@gmail.com>
2021-08-26 18:37:58 +0500

    adding localStorage using redux-persist package

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
-import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 // import reportWebVitals from "./reportWebVitals";
 

9fb884310c360fbffd1d71243ca3cc313cec81ae
JavIonBek <javlonbekshamsiddinov@gmail.com>
2021-08-27 14:10:46 +0500

    adding collection page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import CollectionsOverviewComponent from "../../components/collections-overview/collections-overview.component";
+import CollectionPage from "../collection/collection.component";
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
   <div className="shop-page">
-    <CollectionsOverviewComponent />
+    <Route
+      exact
+      path={`${match.path}`}
+      component={CollectionsOverviewComponent}
+    />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
